38d2c569c963691e600e3bfe0b516eef149e2634
Dylan Dunn <robodylan123@gmail.com>
2015-08-20 16:56:54 -0400

    Added the ability to place and remove blocks

---
+++ Map Editor 2K15/Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Map_Editor_2K15
+{
+    class Block
+    {
+        private int x;
+        private int y;
+        private int ID;
+        private bool hasPhysics;
+
+        public Block(int x, int y, int ID, bool hasPhysics)
+        {
+            this.x = x;
+            this.y = y;
+            this.ID = ID;
+            this.hasPhysics = hasPhysics;
+        }
+
+        public Vector2 getPosition()
+        {
+            return new Vector2(this.x,this.y);
+        }
+
+        public int getID()
+        {
+            return ID;
+        }
+    }
+}
---
+++ Map Editor 2K15/Game1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Map_Editor_2K15
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D mapTexture;
+        int currentID;
+        List<Block> map;
+        Vector2 lastRightClick;
+        Vector2 offset;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            graphics.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = false;
+            IsMouseVisible = true;
+            map = new List<Block>();
+            lastRightClick = new Vector2(-10,-10);
+
+            base.Initialize();
+            for(int i = 0; i < 64; i++)
+            {
+                map.Add(new Block(i, 0, i, true));
+            }
+        }
+
+
+        protected override void LoadContent()
+        {
+            mapTexture = Content.Load<Texture2D>("spritesheet.bmp");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if(Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                Vector2 mouseClickPosition = new Vector2(Mouse.GetState().X / 64, Mouse.GetState().Y / 64);
+                bool canPlace = true;
+                foreach(Block block in map)
+                {
+                    if (block.getPosition() == mouseClickPosition) canPlace = false;
+                }
+                if (canPlace)
+                {
+                    map.Add(new Block(Mouse.GetState().Position.X / 64, Mouse.GetState().Position.Y / 64, currentID, true));
+                }
+            }
+            if(Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                lastRightClick = new Vector2((int)(Mouse.GetState().Position.X / 64), (int)(Mouse.GetState().Position.Y / 64));
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, Matrix.CreateTranslation(offset.X,offset.Y,0));
+            Block blockToRemove = new Block(0,0,0,true);
+            foreach(Block block in map)
+            {
+                if (block.getPosition() == lastRightClick)
+                {
+                    blockToRemove = block;
+                }
+                spriteBatch.Draw(mapTexture, new Vector2((int)block.getPosition().X * 64, (int)block.getPosition().Y * 64), new Rectangle((block.getID() % 8) * 16, block.getID() / 8 * 16,16,16), Color.White, 0, new Vector2(0,0), 4, SpriteEffects.None, 1);
+            }
+            map.Remove(blockToRemove);
+            lastRightClick = new Vector2(-10,-10);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        public void Save(string filename)
+        {
+
+        }
+    }
+}
